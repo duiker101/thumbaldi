7f35c7941a4b432aa11a842ad19d9640d5fad379
Simone Masiero <simone@duiker101.net>
2019-08-07 00:39:44 +0200

    starting to work on the colors

--- src/components/sidebar/ColorSetting.tsx
+++ src/components/sidebar/ColorSetting.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import {IColor} from "../../store/ThumbnailContext";
 
@@ -6,8 +6,14 @@ const Wrapper = styled.div`
 `
 
 const ColorSetting: React.FC<IColor> = (props) => {
+    const [type, setType] = useState('solid')
     return (
         <Wrapper>
+            <select>
+                <option value={'solid'} selected={type==='linear-solid'}>Solid</option>
+                <option value={'gradient'} selected={type==='linear-gradient'}>Gradient</option>
+            </select>
+
         </Wrapper>
     );
 }
